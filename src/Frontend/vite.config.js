7c78d89fe031432ef3f19154a25599e539f05076
ktjayamanna <kaveen.jayamanna@gmail.com>
2026-02-28 20:13:01 +0000

    fixed the port forwarding issue

--- src/Frontend/vite.config.js
+++ src/Frontend/vite.config.js
@@ -5,5 +5,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   manifest: true,
-  
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true
+  }
 })
